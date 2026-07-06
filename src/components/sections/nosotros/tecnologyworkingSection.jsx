3f2dc278bfde991e6aa9abab7c7b5a21344c064e
AlenCamargo <alenjaircamrgo@gmail.com>
2026-07-06 16:59:24 -0500

    detalle texto (Tecnología trabajando para el crecimiento de tu empresa.)

--- src/components/sections/nosotros/tecnologyworkingSection.jsx
+++ src/components/sections/nosotros/tecnologyworkingSection.jsx
@@ -4,8 +4,8 @@ export default function TecnologyWorkingSection() {
   return (
     <section className="py-20">
       <div className="max-w-5xl mx-auto px-4">
-        <h2 className="mt-1 text-white text-3xl md:text-5xl font-bold text-center">
-          Tecnología trabajando para el crecimiento de tu empresa.
+        <h2 className=" text-white text-2xl md:text-4xl font-bold text-center">
+          Tecnología trabajando <br /> para el crecimiento de tu empresa.
         </h2>
       </div>
     </section>
